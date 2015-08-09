68b7713d71348856f89d95a407bda0fd09ba3b36
shivanir <shivanir@INshivanir.local>
2015-08-09 12:30:47 +0530

    |00063| Added the functionality to the CheckOut class when book is available

--- test/com/twu/biblioteca/LibraryOperationView/CheckOutTest.java
+++ test/com/twu/biblioteca/LibraryOperationView/CheckOutTest.java
@@ -1,6 +1,23 @@
 package com.twu.biblioteca.LibraryOperationView;
 
 
+import com.twu.biblioteca.Model.Library;
+import com.twu.biblioteca.Model.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class CheckOutTest {
+    @Test
+    public void returnsTrueIfTheBookIsAvailableInTheAvailableBookList() {
+        Library library = mock(Library.class);
+        User user = mock(User.class);
+        CheckOut checkOut = new CheckOut(library, user);
+
+        when(library.checkoutBook("www", user)).thenReturn(true);
+
+        assertEquals(true, checkOut.execute("www"));
+    }
 
 }
